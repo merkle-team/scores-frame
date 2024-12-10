8205ca20480b236ed4bcb281c6568f6ab62e77b4
Goksu Toprak <19259594+goksu@users.noreply.github.com>
2024-12-10 12:27:51 -0800

    API-layer for rewards (#4)
    Includes an abstract layer and a specific API layer mirroring our
    Warpcast data hooks for this un-authed frame context.
    
    Relates WARP-4025

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,32 @@
 import sdk, { FrameContext } from '@farcaster/frame-sdk';
 import React from 'react';
 
-import { Button } from '../components/ui/button';
+import { Button } from '@/components/ui/button';
 import {
   Card,
   CardContent,
   CardFooter,
   CardHeader,
-} from '../components/ui/card';
+} from '@/components/ui/card';
+import { api } from '@/lib/api';
 
 // eslint-disable-next-line import/no-default-export
 export default function Home() {
   const [isSDKLoaded, setIsSDKLoaded] = React.useState(false);
   const [context, setContext] = React.useState<FrameContext>();
+  const [rewards, setRewards] = React.useState<unknown>();
 
   React.useEffect(() => {
     const load = async () => {
-      setContext(await sdk.context);
+      const ctx: FrameContext = await sdk.context;
+      setContext(ctx);
       sdk.actions.ready({
         disableNativeGestures: false,
       });
+
+      if (typeof ctx !== 'undefined') {
+        setRewards(await api.getCreatorRewardsForUser({ fid: 302 }));
+      }
     };
     if (sdk && !isSDKLoaded) {
       setIsSDKLoaded(true);
@@ -40,6 +47,11 @@ export default function Home() {
         {typeof context === 'undefined'
           ? 'No frame context'
           : JSON.stringify(context.user)}
+      </CardContent>
+      <CardContent>
+        {typeof rewards === 'undefined'
+          ? 'No frame rewards'
+          : JSON.stringify(rewards)}
       </CardContent>
       <CardFooter>
         <Button>Click me</Button>
