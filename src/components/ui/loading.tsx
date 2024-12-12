21e1f57a568909db3f32e50ee92c981a4e836971
Goksu Toprak <19259594+goksu@users.noreply.github.com>
2024-12-12 11:42:43 -0800

    Match the loading splash to the frame image size rendered on Warpcast client (#14)
    Relates WARP-4025

--- src/components/ui/loading.tsx
+++ src/components/ui/loading.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import SplashImage from '../../../public/splash.png';
 
+const FRAME_SPLASH_IMAGE_SIZE = 88;
+
 function Loading() {
   return (
     <div className="animate-pulse w-full items-center justify-center flex flex-col flex-grow h-[calc(100vh-6rem)]">
@@ -9,8 +11,8 @@ function Loading() {
       <img
         src={SplashImage.src}
         alt="Warpcast Rewards splash screen image"
-        width={80}
-        height={80}
+        width={FRAME_SPLASH_IMAGE_SIZE}
+        height={FRAME_SPLASH_IMAGE_SIZE}
       />
     </div>
   );
