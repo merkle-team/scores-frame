3f1330b1aa42c1c7e10a7bedd2e5894af127684b
Goksu Toprak <19259594+goksu@users.noreply.github.com>
2024-12-12 14:10:31 -0800

    Move to a local Inter font to avoid content shift due to swap and update splash (#18)
    Relates WARP-4025

--- src/components/ui/loading.tsx
+++ src/components/ui/loading.tsx
@@ -6,11 +6,13 @@ const FRAME_SPLASH_IMAGE_SIZE = 88;
 
 function Loading() {
   return (
-    <div className="animate-pulse w-full items-center justify-center flex flex-col flex-grow h-[calc(100vh-6rem)]">
+    <div className="inset-0 absolute w-full items-center justify-center flex flex-col flex-grow h-full">
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
         src={SplashImage.src}
         alt="Warpcast Rewards splash screen image"
+        className="animate-pulse"
+        style={{ marginTop: `-${FRAME_SPLASH_IMAGE_SIZE}px` }}
         width={FRAME_SPLASH_IMAGE_SIZE}
         height={FRAME_SPLASH_IMAGE_SIZE}
       />
