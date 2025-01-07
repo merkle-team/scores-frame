8e56285362d6711dbb7e81fdd8cdf14de0bf3ba3
Goksu Toprak <goksu@merklemanufactory.com>
2025-01-07 14:26:30 -0800

    Update hard-coded tiers for the new week of rewards
    These are going to start coming from the server but let's do one
    more quick update on this by hand.

--- src/components/app/rewardTiersPill.tsx
+++ src/components/app/rewardTiersPill.tsx
@@ -19,32 +19,32 @@ function RewardTiersPill() {
           <Card className="flex flex-col mb-4 mx-4">
             <div className="flex flex-row border-b p-3">
               <div className="flex flex-col ml-2 space-y-0.5">
-                <div className="font-semibold text-sm">Tier 1 – $200 Prize</div>
+                <div className="font-semibold text-sm">Tier 1 – $270 Prize</div>
                 <div className="text-muted text-sm">Top 5 winners</div>
               </div>
             </div>
             <div className="flex flex-row border-b p-3">
               <div className="flex flex-col ml-2 space-y-0.5">
-                <div className="font-semibold text-sm">Tier 2 – $50 Prize</div>
+                <div className="font-semibold text-sm">Tier 2 – $70 Prize</div>
                 <div className="text-muted text-sm">Next 20 winners</div>
               </div>
             </div>
             <div className="flex flex-row border-b p-3">
               <div className="flex flex-col ml-2 space-y-0.5">
-                <div className="font-semibold text-sm">Tier 3 – $20 Prize</div>
+                <div className="font-semibold text-sm">Tier 3 – $30 Prize</div>
                 <div className="text-muted text-sm">Next 75 winners</div>
               </div>
             </div>
             <div className="flex flex-row border-b p-3">
               <div className="flex flex-col ml-2 space-y-0.5">
-                <div className="font-semibold text-sm">Tier 4 – $15 Prize</div>
+                <div className="font-semibold text-sm">Tier 4 – $20 Prize</div>
                 <div className="text-muted text-sm">Next 100 winners</div>
               </div>
             </div>
             <div className="flex flex-row p-3">
               <div className="flex flex-col ml-2 space-y-0.5">
                 <div className="font-semibold text-sm">Tier 5 – $10 Prize</div>
-                <div className="text-muted text-sm">Next 200 winners</div>
+                <div className="text-muted text-sm">Next 300 winners</div>
               </div>
             </div>
           </Card>
