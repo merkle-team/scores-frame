7ee803f7d24cc58aee1cb21890bcf64dc89de1cc
Akshaan Kakar <akshaan.crackers@gmail.com>
2025-11-03 22:56:45 -0800

    Temporarily allow displaying 0 scores

--- src/lib/formatters.ts
+++ src/lib/formatters.ts
@@ -1,9 +1,7 @@
 const MIN_SCORE_FORMATTER_TARGET = 1e3 / 2;
 
 function formatScore({ score }: { score: number }) {
-  return score < MIN_SCORE_FORMATTER_TARGET
-    ? `< ${MIN_SCORE_FORMATTER_TARGET.toLocaleString()}`
-    : score.toLocaleString();
+  return score.toLocaleString();
 }
 
 export { formatScore };
